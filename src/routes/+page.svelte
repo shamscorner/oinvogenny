ac6a276ea9d4b315c25ac87274d6dcdfd8f8857d
Shamim Hossain <hossains159@gmail.com>
2023-02-11 13:46:57 +0600

    chore: install tailwind prettier plugin

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,11 @@
-<h1 class="text-3xl font-bold underline bg-blue-400">
-  Hello world!
+<h1
+	class="mt-4 h-5 bg-blue-400 pt-4 pb-6 text-3xl font-bold underline"
+>
+	Hello world!
 </h1>
 
 <style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
+	:global(html) {
+		background-color: theme(colors.gray.100);
+	}
 </style>
