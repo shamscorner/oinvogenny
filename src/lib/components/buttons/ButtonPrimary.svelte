7f22ba85012f10615b1b3ace1b4155bf10505c67
Shamim Hossain <hossains159@gmail.com>
2023-02-12 13:13:20 +0600

    chore: update global buttons focus behavior

--- src/lib/components/buttons/ButtonPrimary.svelte
+++ src/lib/components/buttons/ButtonPrimary.svelte
@@ -6,7 +6,7 @@
 
 <button
 	type="button"
-	class="inline-flex items-center rounded-md min-w-[100px] justify-center border border-transparent bg-pink-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-pink-700 focus:outline-none focus:ring-2 focus:ring-pink-500 focus:ring-offset-2"
+	class="inline-flex items-center rounded-md min-w-[100px] justify-center border border-transparent bg-pink-600 px-4 py-2 text-base font-medium text-white shadow-sm hover:bg-pink-700 focus:outline-none focus-visible:ring-2 focus-visible:ring-pink-500 focus-visible:ring-offset-2 dark:focus-visible:ring-offset-gray-800"
 	on:click={() => dispatch('click')}
 >
 	<slot />
