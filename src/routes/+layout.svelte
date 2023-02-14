bde837e1d86cdc9d031d3be524ab73da68ee0c3f
Shamim Hossain <hossains159@gmail.com>
2023-02-14 22:24:58 +0600

    fix: background flashing issue on app startup

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,6 +14,7 @@
 	import { initIndexDB, type IndexedDBSchemaType } from '$lib/indexDB';
 	import { arrayBufferToFile } from '$lib/helpers';
 	import type { IDBPDatabase } from 'idb';
+	import AppBackground from '$lib/components/AppBackground.svelte';
 
 	let idxDB: IDBPDatabase<IndexedDBSchemaType> | undefined;
 	let isPageLoader = true;
@@ -60,6 +61,8 @@
 		};
 	}
 </script>
+
+<AppBackground />
 
 <div
 	id="overflowed-container"
