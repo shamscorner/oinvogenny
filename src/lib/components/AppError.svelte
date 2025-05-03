26dbcb4b6765bb2b6adf29632de2c658f4b632f6
Shamim <hossains159@gmail.com>
2025-05-03 12:19:27 +0600

    feat: add 404 route

---
+++ src/lib/components/AppError.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+	import Image404 from '../assets/404.svg';
+</script>
+
+<div class="flex h-full items-center justify-center">
+	<img src={Image404} alt="404 - not found" class="aspect-video" />
+</div>
---
+++ src/routes/+error.svelte
@@ -0,0 +1,5 @@
+<script lang="ts">
+	import AppError from '$lib/components/AppError.svelte';
+</script>
+
+<AppError />
